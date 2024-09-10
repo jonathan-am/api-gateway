0e4bbf236d666d2582e9602f86c2d6333b551300
Jonathan <alanjonathan41@gmail.com>
2024-09-10 20:12:56 -0300

    isolado redis start no redis.config.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import app from './app.js';
-import { initRedis } from './client/redis.client.js';
-import { createClient } from "redis";
 import logger from './config/logger.js';
+import { startRedis } from './config/redis.config.js';
 
-const start = async () => {
-    try {
-        logger.info('Iniciando conexao com o redis!')
-        const redis = createClient({url: 'redis://127.0.0.1:6379'});
-        await redis.connect();
-        initRedis(redis);
-        logger.info('Successo ao se conectar com o redis!')
-    }catch(error) {
-        logger.error('Erro ao conectar com redis | message:', error)
-    }
-}
-
-start();
+startRedis();
 
 app.listen(3000, ()=> logger.info('Servidor iniciado na porta 3000'))
